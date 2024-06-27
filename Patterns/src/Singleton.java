86dc478d79f1933289089d8329cc378a7721780a
Kapparov Murat <paprim2003@gmail.com>
2024-06-27 15:49:13 +0500

    singleton

---
+++ Patterns/src/Singleton.java
@@ -0,0 +1,20 @@
+public class Singleton {
+    private static volatile Singleton instance;
+    private String data;
+
+    private Singleton(String data){
+        this.data = data;
+    }
+    public static Singleton getInstance(String data){
+        Singleton result = instance;
+        if (result == null){
+            synchronized (Singleton.class){
+                if (result == null){
+                    instance = result = new Singleton(data);
+                }
+                return instance;
+            }
+        }
+        return result;
+    }
+}
